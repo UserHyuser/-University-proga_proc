6d2c5085c3567791572f2c12b010d672aa06fe62
s0ul701 <spirit_and_soul@mail.ru>
2019-03-24 22:51:43 +0700

    Created new function NumberOfYears

--- src/ProgLg.kt
+++ src/ProgLg.kt
@@ -26,7 +26,8 @@ fun InDataProg(str: String): ProgLg {
 
 fun OutDataProg(progLg: ProgLg, fileWriter: FileWriter) {
     fileWriter.write("Language type: ${progLg.langType}\n" +
-            "Year creation: ${progLg.creationYear}\n"
+            "Year creation: ${progLg.creationYear}\n" +
+            "Quantity years from creation: ${NumberOfYears(progLg)}\n"
     )
     when (progLg.langType) {
         LangType.PROCEDURE -> OutDataProc(procLg = progLg.procLg,
@@ -35,3 +36,5 @@ fun OutDataProg(progLg: ProgLg, fileWriter: FileWriter) {
             fileOut = fileWriter)
     }
 }
+
+fun NumberOfYears(progLg: ProgLg?): Int = 2018 - progLg!!.creationYear!!.toInt()
